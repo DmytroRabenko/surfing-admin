d28bd6bd91e92e46da5d0ac6e98765d45de6aa3e
G1Lroy <potapow.ilay22@gmail.com>
2024-04-16 16:31:06 +0300

    Create starting structure

---
+++ src/pages/Calendar.tsx
@@ -0,0 +1,11 @@
+
+
+type Props = {}
+
+const Calendar = (props: Props) => {
+  return (
+    <div>Calendar</div>
+  )
+}
+
+export default Calendar
--- src/pages/dashboard/dashboard.tsx
+++
@@ -1,4 +0,0 @@
-const Dashbourd = () => {
-  return <div>Dashbourd</div>;
-};
-export default Dashbourd;
