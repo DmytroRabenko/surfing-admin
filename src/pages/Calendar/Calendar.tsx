d8e11ba1049ad52d6d1d4134b5501087dc49bb62
Vladislav <104913853+Adwasar@users.noreply.github.com>
2024-04-17 22:23:02 +0300

    feat: init schedule library

--- src/pages/Calendar/Calendar.tsx
+++ src/pages/Calendar/Calendar.tsx
@@ -1,9 +1,36 @@
-import { Header } from "src/components/calendar/Header";
+import { useState } from 'react';
+import { Scheduler } from '@bitnoi.se/react-scheduler';
+
+import '@bitnoi.se/react-scheduler/dist/style.css';
+import s from './Calendar.module.scss';
+
+import mockedSchedulerData from './mock-data'
 
 const Calendar = () => {
+  const [filterButtonState, setFilterButtonState] = useState(0);
+
   return (
     <>
-      <Header />
+      <div className={s.calendar}>
+        <div className={s.calendar__content}>
+          <Scheduler
+            data={mockedSchedulerData}
+            onRangeChange={newRange => console.log(newRange)}
+            onTileClick={clickedResource => console.log(clickedResource)}
+            onItemClick={item => console.log(item)}
+            onFilterData={() => {
+              setFilterButtonState(1);
+            }}
+            onClearFilterData={() => {
+              setFilterButtonState(0);
+            }}
+            config={{
+              zoom: 0,
+              filterButtonState,
+            }}
+          />
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/pages/Calendar/mock-data.ts
@@ -0,0 +1,116 @@
+import { SchedulerData } from '@bitnoi.se/react-scheduler';
+
+const mockedSchedulerData: SchedulerData = [
+  {
+    id: '070ac5b5-8369-4cd2-8ba2-0a209130cc60',
+    label: {
+      icon: 'https://picsum.photos/24',
+      title: 'board 1',
+      subtitle: 'Frontend Developer',
+    },
+    data: [
+      {
+        id: '8b71a8a5-33dd-4fc8-9caa-b4a584ba3762',
+        startDate: new Date('2024-04-17T15:31:24.272Z'),
+        endDate: new Date('2024-04-18T10:28:22.649Z'),
+        occupancy: 3600,
+        title: 'Project A',
+        subtitle: 'Subtitle A',
+        description: 'array indexing Salad West Account',
+        bgColor: 'rgba(93, 181, 93, 0.5)',
+      },
+      {
+        id: '22fbe237-6344-4c8e-affb-64a1750f33bd',
+        startDate: new Date('2024-04-20T15:31:24.272Z'),
+        endDate: new Date('2024-04-22T10:28:22.649Z'),
+        occupancy: 2852,
+        title: 'Project B',
+        subtitle: 'Subtitle B',
+        description: 'Tuna Home pascal IP drive',
+        bgColor: 'rgba(93, 181, 93, 0.5)',
+      },
+    ],
+  },
+  {
+    id: '070ac5b5-8369-4cd1-8ba2-0a209130cc60',
+    label: {
+      icon: 'https://picsum.photos/24',
+      title: 'board 2',
+      subtitle: 'Frontend Developer',
+    },
+    data: [
+      {
+        id: '8b71a8a5-33dd-4fc8-9caa-b4a584ba3762',
+        startDate: new Date('2024-04-14T15:31:24.272Z'),
+        endDate: new Date('2024-04-18T10:28:22.649Z'),
+        occupancy: 3600,
+        title: 'Project A',
+        subtitle: 'Subtitle A',
+        description: 'array indexing Salad West Account',
+        bgColor: 'rgba(93, 181, 93, 0.5)',
+      },
+      {
+        id: '22fbe237-6344-4c8e-affb-64a1750f33bd',
+        startDate: new Date('2024-04-21T15:31:24.272Z'),
+        endDate: new Date('2024-04-23T10:28:22.649Z'),
+        occupancy: 2852,
+        title: 'Project B',
+        subtitle: 'Subtitle B',
+        description: 'Tuna Home pascal IP drive',
+        bgColor: 'rgba(93, 181, 93, 0.5)',
+      },
+      {
+        id: '22fbe237-6344-4c8e-xffb-64a1750f33bd',
+        startDate: new Date('2024-04-25T15:31:24.272Z'),
+        endDate: new Date('2024-04-30T10:28:22.649Z'),
+        occupancy: 2852,
+        title: 'Project B',
+        subtitle: 'Subtitle B',
+        description: 'Tuna Home pascal IP drive',
+        bgColor: 'rgba(93, 181, 93, 0.5)',
+      },
+    ],
+  },
+  {
+    id: '070ac5b5-8369-4cd1-8ba2-0a209130cc60',
+    label: {
+      icon: 'https://picsum.photos/24',
+      title: 'board 3',
+      subtitle: 'Frontend Developer',
+    },
+    data: [
+      {
+        id: '8b71a8a5-33dd-4fc8-9caa-b4a584ba3762',
+        startDate: new Date('2024-05-13T15:31:24.272Z'),
+        endDate: new Date('2024-05-18T10:28:22.649Z'),
+        occupancy: 3600,
+        title: 'Project A',
+        subtitle: 'Subtitle A',
+        description: 'array indexing Salad West Account',
+        bgColor: 'rgba(202, 202, 202, 0.5)',
+      },
+    ],
+  },
+  {
+    id: '070ac5b5-8369-4cd1-8ba2-0a209130cc60',
+    label: {
+      icon: 'https://picsum.photos/24',
+      title: 'board 4',
+      subtitle: 'Frontend Developer',
+    },
+    data: [
+      {
+        id: '8b71a8a5-33dd-4fc8-9caa-b4a584ba3762',
+        startDate: new Date('2024-05-11T15:31:24.272Z'),
+        endDate: new Date('2024-05-23T10:28:22.649Z'),
+        occupancy: 3600,
+        title: 'Project A',
+        subtitle: 'Subtitle A',
+        description: 'array indexing Salad West Account',
+        bgColor: 'rgba(93, 181, 93, 0.5)',
+      }
+    ],
+  },
+];
+
+export default mockedSchedulerData;
