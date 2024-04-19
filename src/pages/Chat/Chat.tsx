4bfad18754d655692ab0c0e3333a135eba4daf97
Kostaintyn Kikinov <kikinov.k@gmail.com>
2024-04-19 15:23:56 +0300

    feat: add Chat component

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -1,8 +1,24 @@
+import ChatRoom from 'src/components/Chat/ChatRoom';
+import UsersList from 'src/components/Chat/UsersList';
+
+import style from './Chat.module.scss';
+
+import { data } from './data';
 
 const Chat = () => {
+  const handlerSelectUser = (uid: string) => {
+    console.log('handlerSelectUser', uid)
+  }
   return (
-    <div>Chat</div>
-  )
+    <div className={style.chat}>
+      <div className={style.chat__users}>
+        <UsersList contacts={data} selectUser={handlerSelectUser}/>
+      </div>
+      <div className={style.chat__room}>
+        <ChatRoom />
+      </div>
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
---
+++ src/type/chat.ts
@@ -0,0 +1,20 @@
+export interface User {
+  uid: string;
+  photoURL: string;
+  name: string;
+  lastMessage: string;
+  dateLastMassage: string;
+  isOpen?: boolean;
+  isSent: boolean;
+  isRead: boolean;
+}
+
+export interface Message {
+  id: string;
+  text: string;
+  date: string;
+  isOut?: boolean;
+  isIn?: boolean;
+  isSent?: boolean;
+  isRead?: boolean;
+}
